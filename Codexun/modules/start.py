e7173164ab80c2ac519a7af733eccde0ea60eea3
bhkvlldu <104181159+bhkvlldu@users.noreply.github.com>
2022-06-21 22:52:10 +0200

    Add files via upload

--- Codexun/modules/start.py
+++ Codexun/modules/start.py
@@ -1,6 +1,5 @@
 import asyncio
-from pytz import timezone
-from datetime import datetime
+
 from pyrogram import Client, filters, __version__ as pyrover
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pytgcalls import (__version__ as pytover)
@@ -13,7 +12,7 @@ from Codexun.config import BOT_NAME
 from Codexun.config import START_IMG
 
 @Client.on_message(command("start") & filters.private & ~filters.edited)
-async def strt_(client: Client, message: Message):
+async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://t.me/{BOT_USERNAME}",
         caption=f"""**مرحبا {message.from_user.mention()}** 👋
